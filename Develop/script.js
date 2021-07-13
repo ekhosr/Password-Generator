43277f38e1761c655c1c8e431a20a0b9b3a52c1f
Ehsan Khosravi <ekhosr@gmail.com>
2021-07-13 18:23:09 -0400

    update code: creat character array based on user's selection

--- Develop/script.js
+++ Develop/script.js
@@ -46,11 +46,35 @@ if (askNumeric == false && askLower == false && askUpper == false && askSpecial 
   
 };
 
-var numric = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var specialCharacter = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "^", "_", "`", "{", "|", "}", "~"];
+//Declaring the password charactrers in arrays
+var numricArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var upperCaseArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lowerCaseArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var specialCharacterArray = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "^", "_", "`", "{", "|", "}", "~"];
+var passwordArray = [];
 
 
+//checks the if statement for numeric characters, if returns true, it adds the items of numericArray to PasswordArray
+if (askNumeric == true) {
+  passwordArray = passwordArray.concat(numricArray);
+  console.log(passwordArray);
+}
 
+//checks the if statement for upper case characters, if returns true, it adds the items of upperCaseArray to PasswordArray
+if (askUpper == true) {
+  passwordArray = passwordArray.concat(upperCaseArray);
+  console.log(passwordArray);
+}
+
+//checks the if statement for lower case characters, if returns true, it adds the items of lowerCaseArray to PasswordArray
+if (askLower == true) {
+  passwordArray = passwordArray.concat(lowerCaseArray);
+  console.log(passwordArray);
+}
+
+//checks the if statement for special characters, if returns true, it adds the items of specialCharacterArray to PasswordArray
+if (askSpecial == true) {
+  passwordArray = passwordArray.concat(specialCharacterArray);
+  console.log(passwordArray);
+}
 
