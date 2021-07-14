d31772b5ca6cd18f4f637926bbdcb7f40d442410
Ehsan Khosravi <ekhosr@gmail.com>
2021-07-14 18:16:28 -0400

    removed arrays, and replaced with character varaiables

--- Develop/script.js
+++ Develop/script.js
@@ -15,66 +15,83 @@ generateBtn.addEventListener("click", writePassword);
 
 //my code starts here
 
-//Declaring vaiable for password length
-var passwordLength = 0;
 
 //keep asking user for password length until user enter a NUMBER between 8 to 128 
- while (passwordLength < 8 || passwordLength >128 || isNaN(passwordLength) ) {
-  passwordLength = window.prompt("Please Enter a <Number> between 8 to 128 for Your Password Length:");
+while (passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength)) {
+  var passwordLength = window.prompt("Please Enter a <Number> between 8 to 128 for Your Password Length:");
   console.log(passwordLength);
- };
 
- //Check to see if user wants numeric characters in her/his password
+};
+
+
+//Check to see if user wants numeric characters in her/his password
 var askNumeric = window.confirm("Do You Want Numeric Characters in Your Password?");
 console.log(askNumeric);
 
 
- //Check to see if user wants Upper Case characters in her/his password
- var askUpper = window.confirm("Do You Want Upper Case Characters in Your Password?");
- console.log(askUpper);
+//Check to see if user wants Upper Case characters in her/his password
+var askUpper = window.confirm("Do You Want Upper Case Characters in Your Password?");
+console.log(askUpper);
 
- //Check to see if user wants Lower Case characters in her/his password
- var askLower = window.confirm("Do You Want Lower Case Characters in Your Password?");
- console.log(askLower);
+//Check to see if user wants Lower Case characters in her/his password
+var askLower = window.confirm("Do You Want Lower Case Characters in Your Password?");
+console.log(askLower);
 
- //Check to see if user wants Special characters in her/his password
-  var askSpecial = window.confirm("Do You Want Special Characters in Your Password?");
-  console.log(askSpecial);
+//Check to see if user wants Special characters in her/his password
+var askSpecial = window.confirm("Do You Want Special Characters in Your Password?");
+console.log(askSpecial);
 
 if (askNumeric == false && askLower == false && askUpper == false && askSpecial == false) {
   alert("You Should at lease choose a numeric, Upper Case, Lower Case or Special Character ");
-  
+
 };
 
-//Declaring the password charactrers in arrays
-var numricArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var upperCaseArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var lowerCaseArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var specialCharacterArray = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "^", "_", "`", "{", "|", "}", "~"];
-var passwordArray = [];
+//Declaring the password charactrers
+var numric = "0123456789";
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+var specialChar = "!@#$%^&*()_+-=[]\;',./{}|:<>?";
+
+//functin to generate password
+function generatePassword() {
+
+  //Declare a container to put the characters which returned True from if statements  
+   var passwordContainer = '';
+  
+  //checks the if statement for numeric characters, if returns true, it adds numeric characters to the container
+  if (askNumeric == true) {
+    passwordContainer = passwordContainer + numric;
+    console.log(passwordContainer);
+
+  };
+
+  //checks the if statement for upper case characters, if returns true, it adds upper case characters to the container
+  if (askUpper == true) {
+    passwordContainer = passwordContainer + upperCase;
+    console.log(passwordContainer);
+  };
+
+  //checks the if statement for lower case characters, if returns true, it adds lower case characters to the container
+  if (askLower == true) {
+    passwordContainer = passwordContainer + lowerCase;
+    console.log(passwordContainer);
+  };
+
+  //checks the if statement for special characters, if returns true, it adds special  characters to the container
+  if (askSpecial == true) {
+    passwordContainer = passwordContainer + specialChar;
+    console.log(passwordContainer);
+  };
+   
+  //gets random characters from container and add it to results until the length is smaller than user choice for password length
+  var passwordResult = '';
+  for (i = 0; i < passwordLength; i++) {
+    var randomChar = Math.floor(Math.random() * passwordContainer.length);
+    passwordResult += passwordContainer[randomChar];
+  }
+   
+  return passwordResult;
+  console.log(passwordResult);
 
 
-//checks the if statement for numeric characters, if returns true, it adds the items of numericArray to PasswordArray
-if (askNumeric == true) {
-  passwordArray = passwordArray.concat(numricArray);
-  console.log(passwordArray);
-}
-
-//checks the if statement for upper case characters, if returns true, it adds the items of upperCaseArray to PasswordArray
-if (askUpper == true) {
-  passwordArray = passwordArray.concat(upperCaseArray);
-  console.log(passwordArray);
-}
-
-//checks the if statement for lower case characters, if returns true, it adds the items of lowerCaseArray to PasswordArray
-if (askLower == true) {
-  passwordArray = passwordArray.concat(lowerCaseArray);
-  console.log(passwordArray);
-}
-
-//checks the if statement for special characters, if returns true, it adds the items of specialCharacterArray to PasswordArray
-if (askSpecial == true) {
-  passwordArray = passwordArray.concat(specialCharacterArray);
-  console.log(passwordArray);
-}
-
+};
